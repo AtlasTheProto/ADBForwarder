7f96a858bb0154b9c803e37a196fa9db1dafeb2b
Atlas <seanomahoney116@gmail.com>
2021-09-29 03:03:21 -0400

    Refactor ADB detection, download automatically

--- Program.cs
+++ Program.cs
@@ -1,34 +1,38 @@
 ﻿using System;
 using SharpAdbClient;
 using System.Net;
+using System.IO;
+using Ionic.Zip;
 
 namespace ADBForwarder
 {
     class Program
     {
-        const string sidequest = "\\Programs\\SideQuest\\resources\\app.asar.unpacked\\build\\platform-tools\\adb.exe";
-        static string localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-        static string adbPath = localAppData + sidequest; //i fucking hate this but path.combine was not having it
+        //const string sidequest = "\\Programs\\SideQuest\\resources\\app.asar.unpacked\\build\\platform-tools\\adb.exe";
+        //static string localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        static string adbPath = Path.GetTempPath() + "\\adb\\platform-tools\\adb.exe";
+        static Uri uri = new Uri("https://dl.google.com/android/repository/platform-tools-latest-windows.zip");
+
 
         static void Main(string[] args)
         {
-            Console.WriteLine("starting");
-
             var endPoint = new IPEndPoint(IPAddress.Loopback, AdbClient.AdbServerPort);
 
             AdbServer server = new AdbServer();
-            try
+
+            if(!File.Exists(adbPath))
+            {
+                Console.WriteLine("ADB not found, downloading in the background...");
+                DownloadADB();
+                Console.WriteLine("Download Successful, starting ADB Daemon...");
+                var result = server.StartServer(adbPath, restartServerIfNewer: false);
+                
+            }
+            else
             {
                 var result = server.StartServer(adbPath, restartServerIfNewer: false);
-                Console.WriteLine("ADB Daemon Started");
+                Console.WriteLine("Starting ADB Daemon...");
             }
-            catch
-            {
-                Console.WriteLine("SideQuest not installed.");
-                Console.ReadKey();
-                return;
-            }
-            
 
             var client = new AdbClient();
             client.Connect(endPoint);
@@ -54,6 +58,20 @@ namespace ADBForwarder
                             //TODO: make this better
                         }
                     }
+                }
+            }
+        }
+
+        static void DownloadADB()
+        {
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(uri, "adb.zip");
+
+                using (ZipFile zip = ZipFile.Read("adb.zip"))
+                {
+                    zip.ExtractAll(Path.GetTempPath() + "\\adb",
+                    ExtractExistingFileAction.DoNotOverwrite);
                 }
             }
         }
