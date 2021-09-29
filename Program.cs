df74040ff9c2e4f2a0c308050841610daaf0b053
Atlas <seanomahoney116@gmail.com>
2021-09-29 18:57:06 -0400

    refactor 2

--- Program.cs
+++ Program.cs
@@ -2,23 +2,24 @@
 using SharpAdbClient;
 using System.Net;
 using System.IO;
+using System.Collections.Generic;
 using Ionic.Zip;
+using Usb.Events;
 
 namespace ADBForwarder
 {
     class Program
     {
-        //const string sidequest = "\\Programs\\SideQuest\\resources\\app.asar.unpacked\\build\\platform-tools\\adb.exe";
-        //static string localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
         static string adbPath = Path.GetTempPath() + "\\adb\\platform-tools\\adb.exe";
+        static AdbClient client = new AdbClient();
+        static AdbServer server = new AdbServer();
         static Uri uri = new Uri("https://dl.google.com/android/repository/platform-tools-latest-windows.zip");
-
+        static IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, AdbClient.AdbServerPort);
+        List<DeviceData> devices = new List<DeviceData>();
+        
 
         static void Main(string[] args)
         {
-            var endPoint = new IPEndPoint(IPAddress.Loopback, AdbClient.AdbServerPort);
-
-            AdbServer server = new AdbServer();
 
             if(!File.Exists(adbPath))
             {
@@ -26,7 +27,6 @@ namespace ADBForwarder
                 DownloadADB();
                 Console.WriteLine($"Download Successful!\nLocated at: {adbPath}\nStarting ADB Daemon...");
                 var result = server.StartServer(adbPath, restartServerIfNewer: false);
-                
             }
             else
             {
@@ -34,34 +34,51 @@ namespace ADBForwarder
                 Console.WriteLine("Starting ADB Daemon...");
             }
 
-            var client = new AdbClient();
             client.Connect(endPoint);
+            
+            DeviceMonitor monitor = new DeviceMonitor(new AdbSocket(endPoint));
+            monitor.DeviceConnected += Monitor_DeviceConnected;
+            monitor.DeviceDisconnected += Monitor_DeviceDisconnected;
+            
+            monitor.Start();
 
-            var devices = client.GetDevices();
-
+            List<DeviceData> devices = client.GetDevices();
 
             while (true)
             {
-                System.Threading.Thread.Sleep(1000);
-                foreach (var device in devices)
-                {
-                    if (device.Name == "hollywood")
-                    {
-                        try
-                        {
-                            client.CreateForward(device, 9943, 9943);
-                            client.CreateForward(device, 9944, 9944);
-                        }
-                        catch
-                        {
-                            //do nothing
-                            //TODO: make this better
-                        }
-                    }
-                }
+                // Main thread needs to stay alive, 100ms is acceptable idle time
+                System.Threading.Thread.Sleep(100);
             }
         }
 
+        private static void Monitor_DeviceDisconnected(object sender, DeviceDataEventArgs e)
+        {
+            Console.WriteLine($"Event: Disconnection\n Device: {e.Device.Serial}");
+        }
+
+        private static void Monitor_DeviceConnected(object sender, DeviceDataEventArgs e)
+        {
+
+            Console.WriteLine($"Event: Connection\n Device: {e.Device.Serial}");
+            System.Threading.Thread.Sleep(1000); // dont do shit immediately
+            Forward();
+        }
+
+        static void Forward()
+        {
+            List<DeviceData> devices = client.GetDevices();
+
+            foreach (var device in devices)
+            {
+                if(device.Name == "hollywood")
+                {
+                    client.CreateForward(device, 9943, 9943);
+                    client.CreateForward(device, 9944, 9944);
+                    Console.WriteLine("Forwarded!");
+
+                }
+            }
+        }
         static void DownloadADB()
         {
             using (var client = new WebClient())
